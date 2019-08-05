c8e8afefd030869aa660835251062ab88321c1c0
Alethia Quintero <alethiaq22@gmail.com>
2019-08-05 21:21:03 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -74,7 +74,7 @@ attr_accessor :path
     puts "Which song number would you like to play?"
     song_num = gets.chomp.to_i
     if song_num > 0 && song_num <= Song.all.length
-      song = Song.all.sort_by {|song| song.name}
+      song = Song.all.sort {|song| song.name}
       end
       puts "Playing #{song.name} by #{song.artist.name}" if song
 

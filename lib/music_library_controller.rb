3a3d467b6a2abb255086f88fd1f6f235a29cce80
Alethia Quintero <alethiaq22@gmail.com>
2019-08-05 20:38:38 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -33,6 +33,8 @@ attr_accessor :path
     sorted = Song.all.sort_by {|song| song.name}
     sorted.each_with index do |song, index|
       puts "#{index +1 }. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+  end
 
 
 

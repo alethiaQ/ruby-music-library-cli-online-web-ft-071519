5de8da8db2f7f61cf4af78306b0214ff76403cc1
Alethia Quintero <alethiaq22@gmail.com>
2019-08-05 21:32:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,7 +54,7 @@ end
   end
 
   def self.find_by_name(name)
-    #self.all.find {|song| song.name == name}
+    self.all.find {|song| song.name == name}
   end
 
    def self.find_or_create_by_name(name)
